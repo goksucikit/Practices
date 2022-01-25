8a5b02af1ddc9d384b80ff3be5277287759c6742
Goksu Cikit <goksu.cikit@gmail.com>
2022-01-25 14:23:30 +0300

    First Commit

---
+++ click-outside-exercise/README.md
@@ -0,0 +1,7 @@
+# Popping Cards Exercise
+## Cards
+### Here we have four cards made with Bootstrap and all them have a button which triggers a pop-up that show up an image and a description below.
+
+### Once we have a look-see, we can close the inner page by clicking outside & the closing image on the right-top or pressing the key Esc.
+
+ 
---
+++ click-outside-exercise/click-outside-recover.js
@@ -0,0 +1,54 @@
+//Select elements we need
+const cardButtons = document.querySelectorAll('.btn');
+const modalOuter = document.querySelector('.modal-outer');
+const modalInner = document.querySelector('.modal-inner');
+const closeButton = document.querySelector('.delete');
+
+//HANDLER FUNCTION
+const handleClick = event => {
+    const button = event.currentTarget;
+    const card = button.closest('.card');
+
+    //grab the image source, description and text content from the cards
+    const imgSrc = card.querySelector('img').src;
+    const desc = card.querySelector('img').dataset.description;
+    const name = card.querySelector('h5').textContent;
+
+    //pop the modal with the new info
+    const modalInnerHTML = `
+        <img 
+        width="600" height="600" 
+        src="${imgSrc.replace('200', '600')}" 
+        alt="${name}"> 
+        <button class="delete">X</button>
+        </img>
+        <p>${desc}</p>
+    `;
+
+    //place inner html to our page
+    const innerRange = document.createRange().createContextualFragment(modalInnerHTML);
+    modalInner.innerHTML = modalInnerHTML;
+    modalOuter.classList.add('modal-outer-open');
+};
+
+cardButtons.forEach(button => {
+    button.addEventListener('click', handleClick);
+});
+
+const closeModal = () => modalOuter.classList.remove('modal-outer-open');
+
+modalOuter.addEventListener('click', function (event) {
+    const isOutside = !event.target.closest('.model-inner');
+
+   if (isOutside) {
+        closeModal();
+   }else if (event.target.closest('.delete')) {
+        closeModal();
+   }
+});
+
+window.addEventListener('keydown', function (event) {
+    if(event.key === 'Escape') {
+        closeModal ();
+    }
+});
