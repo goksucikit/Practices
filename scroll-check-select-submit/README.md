da835151dca3045e3a0de0dabe00afaffc553c05
Goksu Cikit <goksu.cikit@gmail.com>
2022-01-31 00:29:12 +0300

    First commit

---
+++ scroll-check-select-submit/README.md
@@ -0,0 +1,20 @@
+# Scroll-Check-Select-Submit
+
+## Scroll
+### We have a terms & conditions text which we have to scroll the page to read all.
+
+## Check
+### Here we created a checkbox which the users have to check if they are accepting the terms & conditions above. They can not proceed as long as the checkbox hasn't been checked.
+
+### I used Intersection Observer API to enable checkbox section.
+
+## Select
+### In case the checkbox progress has been completed, the users can select one of the items they have been given.
+
+### onchange event and removeAttribute() method make the options enable here.
+
+
+## Submit
+### After all processes has been completed, the disabled and hidden Submit button appears on the bottom of the page. 
+
+### In this part of code block, I applied addEventListener method with the change event.
---
+++ scroll-check-select-submit/observer-exercise.js
@@ -0,0 +1,39 @@
+// Select the elements
+const container = document.querySelector('.container');
+const check = document.querySelector('.form-check-input');
+const select = document.querySelector('.form-select');
+const btn = document.querySelector('.btn');
+
+// IntersectionObserver API for scrolling
+const obCallBack = payload => {
+    if (payload[0].intersectionRatio === 1) {
+        check.disabled = false;
+        observed.unobserve(container.lastElementChild);
+    }; 
+};
+
+const observed = new IntersectionObserver(obCallBack, {
+    root: container,
+    threshold: 1,
+});
+
+observed.observe(container.lastElementChild);
+
+
+//Enabling options after accepting the terms&conditions by clicking the checkbox
+check.onchange = function () {
+    if (this.checked = true) {
+        select.removeAttribute('disabled');
+    };
+};
+
+
+//Make visible and enable the Submit button by choosing an option
+select.addEventListener('change', event => {
+    const value = event.target.value;
+    
+    console.log(value);
+    if (value >= 1) {
+        btn.disabled = false;
+    };
+});
